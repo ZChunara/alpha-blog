2ec91230de7757d6122d1455106fbd934ef552a5
Zobiaa <zomgbia@gmail.com>
2016-06-10 14:29:40 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-root 'pages#home'
-get 'pages/about', to: 'pages#about'
-  # Example of regular route:
+
+  # Example of regularroute:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
